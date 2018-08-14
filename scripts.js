787ec5cb3d464aa9e00420bb09226e447a2e3625
Daniel <dtkahn@gmail.com>
2018-08-14 16:22:12 -0400

    project outline in comments in scripts.js, assets folder and test images added

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Create a classmates object with
+    // Name
+    // Img (link to img)
+
+// Provide 3 wrong names
+    // Create a list of all names (populated from the names in the classmates object)
+
+// Stop classmates who have already been shown from showing again unless all have been shown
+
+// Keep track of score
+    // +1 for every correct answer
+    // -1 for every wrong answer
+
+// Limit play to 60 seconds
+    // Show countdown? (number or bar)
+
+// Display the image of the classmate
+
+// Display all 4 names (1 correct and 3 wrong)
